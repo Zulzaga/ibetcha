711d28ece7c4b80ffed9d272ce7d9b4b852a2e2f
Zulsar Batmunkh <zulsar@mit.edu>
2014-11-09 13:17:47 -0500

    Authentication added

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,16 @@ var User = models.User,
     Bet = models.Bet,
     Milestone = models.Milestone;
 
+// Authenticates the user and redirects to the users login page if necessary.
+function isAuthenticated(req, res, next) {
+    if (req.user) {
+        return next();
+    }
+
+    // If a user is not logged in, redirect to the login page.
+    res.json({success: false, error: "User is not logged in!"});
+};
+
 // GET /users
 // Request parameters:
 //     - none
@@ -15,7 +25,7 @@ var User = models.User,
 //     - success: true if all users were successfully retrieved
 //     - content: TDB
 //     - err: on failure, an error message
-router.get('/', function(req, res) {
+router.get('/', isAuthenticated, function(req, res) {
   res.send('respond with a resource');
 });
 
@@ -26,7 +36,7 @@ router.get('/', function(req, res) {
 //     - success: true if the user was created (and the verification email sent)
 //     - content: TBD
 //     - err: on failure, an error message
-router.post('/login', function(req, res) {
+router.post('/login', isAuthenticated, function(req, res) {
   res.send('respond with a resource');
 });
 
@@ -37,20 +47,20 @@ router.post('/login', function(req, res) {
 //     - success: true if the user's profile is retrieved
 //     - content: TBD
 //     - err: on failure, an error message
-router.get('/:user_id', function(req, res) {
+router.get('/:user_id', isAuthenticated, function(req, res) {
   res.send('respond with a resource');
 });
 
-/*// GET /users/friends/:user_id NOTE: duplication with users/:user_id
+// GET /users/friends/:user_id
 // Request parameters:
 //     - user_id: a String representation of the MongoDB _id of the user
 // Response:
 //     - success: true if the user's friends successfully retrieved
 //     - content: TBD
 //     - err: on failure, an error message
-router.get('/friends/:user_id', function(req, res) {
+router.get('/friends/:user_id', isAuthenticated, function(req, res) {
   res.send('respond with a resource');
-});*/
+});
 
 // GET /users/logout
 // Request parameters/body: (note req.body for forms)
@@ -59,7 +69,7 @@ router.get('/friends/:user_id', function(req, res) {
 //     - success: true if the user is successfully logged out
 //     - content: TBD
 //     - err: on failure, an error message
-router.get('/logout', function(req, res) {
+router.get('/logout', isAuthenticated, function(req, res) {
   res.send('respond with a resource');
 });
 
