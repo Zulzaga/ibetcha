e2650ef1feb79321e77005031cf16084baf84fdc
Zulsar Batmunkh <zulsar@mit.edu>
2014-11-16 17:11:44 -0500

    Edit page updated

--- public/javascripts/controllers/EditBetPageController.js
+++ public/javascripts/controllers/EditBetPageController.js
@@ -1,29 +1,38 @@
 ibetcha.controller('EditBetPageController',
     function($scope, $http, $location, $cookieStore) {
-        $scope.editForm = {};
-        $scope.editForm.friends = [];
-        $http({
-            method:"GET",
-            url: "users/friends/" + $cookieStore.get('user')
-        }).success(function(data, status, headers, config) {
-            $scope.editForm.friends = data.content;
-            console.log("retrieved friends", $scope.editForm.friends);
-        }).error(function(jqXHR, textStatus, err){
-            alert(err);
-        })
+        $http.defaults.headers.post["Content-Type"] = "application/json";
 
-        
-        // $scope.$on("homeClicked", function(event, args) {
-        //     $http({
-        //         method: "GET",
-        //         url: "roadmaps",
-        //     }).success(function(data, status, headers, config) {
-        //         $scope.roadmaps = data.content;
-        //     }).
-        //     error(function(data, status, headers, config) {
-        //         alert(data.err);
-        //     });
-        // });
+        if (!$cookieStore.get('session')) {
+            $location.path('/');
+        } else {
+            $scope.editForm = {};
+            $scope.editForm.friends = [];
+            $http({
+                method:"GET",
+                url: "users/friends/" + $cookieStore.get('user')
+            }).success(function(data, status, headers, config) {
+                $scope.friends = data.content;
+                console.log("retrieved friends", $scope.editForm.friends);
+            }).error(function(jqXHR, textStatus, err){
+                alert(err);
+            })
+
+            $scope.submit = function(){
+                console.log($scope.editForm.monitors);
+                // $http({
+                //     method: "POST",
+                //     url: "bets",
+                //     data: { to: $scope.requestForm.friendEmail },
+                //     }).success(function(data, status, headers, config) {
+                //         console.log(data.content);
+                //         alert("Successfully sent the request!");
+                //         $location.path('/home');
+                //     }).
+                // error(function(data, status, headers, config) {
+                //     alert(data.err);
+                // });
+            }
+        }
 
     }
 
