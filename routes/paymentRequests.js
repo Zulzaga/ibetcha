6e3d8ef449832eefa33accd017ca4da68b96a472
saadiyahhPrivate <saadiyah@mit.edu>
2014-12-01 23:40:25 -0500

    payment Requests

--- routes/paymentRequests.js
+++ routes/paymentRequests.js
@@ -15,21 +15,20 @@ var MonitorRequest = require('../models/MonitorRequest');
 var MoneyRecord = require('../models/MoneyRecord');
 var isAuthenticated = utils.isAuthenticated;
 
-
 // Update the payment to indicate the claim that the user has paid friends
 router.get('/paid/:paymentId/claim', isAuthenticated, function(req, res) {
     console.log("inside serverside paid claim", req.params.paymentId);
 
     mongoose.model('MoneyRecord').processPaymentClaim({_id: req.params.paymentId}, 
-                                    {$set: {requested: true}},
-                                    function(err, code, content){
-                                        if (err) {
-                                            utils.sendErrResponse(res, code, content);      
-                                        }
-                                        else{
-                                            utils.sendSuccessResponse(res, content);
-                                        }
-                                    });
+        {$set: {requested: true}},
+        function(err, code, content){
+            if (err) {
+                utils.sendErrResponse(res, code, content);      
+            }
+            else{
+                utils.sendSuccessResponse(res, content);
+            }
+    });
 
 });
 
@@ -37,35 +36,15 @@ router.get('/paid/:paymentId/claim', isAuthenticated, function(req, res) {
 router.get('/received/:paymentId', isAuthenticated, function(req, res) {
     console.log('inside serverside received');
     mongoose.model('MoneyRecord').confirmPaymentClaim({_id: req.params.paymentId},
-                                    function(err, code, content){
-                                        if (err) {
-                                            utils.sendErrResponse(res, code, content);      
-                                        }
-                                        else{
-                                            utils.sendSuccessResponse(res, content);
-                                        }
-                                    });
-    // MoneyRecord.findOneAndRemove({_id: req.params.paymentId}, 
-    //                                function(err, code, content){
-    //                                     if (err) {
-    //                                         utils.sendErrResponse(res, code, content);      
-    //                                     }
-    //                                     else{
-    //                                         utils.sendSuccessResponse(res, content);
-    //                                     }
-    //                                 });
+        function(err, code, content){
+            if (err) {
+                utils.sendErrResponse(res, code, content);      
+            }
+            else{
+                utils.sendSuccessResponse(res, content);
+            }
+    });
+
 });
-
-// // Gets all monitor requests current user received.
-// router.get('/', isAuthenticated, function(req, res) {
-//     MoneyRecord.find({ to: req.user._id }).populate('from to').exec(function (err, requests) {
-//         if (err) {
-//             utils.sendErrResponse(res, 500, 'There was an error! Could not get requests.')
-//         } else {
-//             utils.sendSuccessResponse(res, requests);
-//         }
-//     });
-// });
-
 
 module.exports = router;
