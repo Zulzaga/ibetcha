8c0bcb98e14eae2b5e01440e59655a567af8267b
Zulsar Batmunkh <zulsar@mit.edu>
2014-12-02 23:16:56 -0500

    S

--- routes/index.js
+++ routes/index.js
@@ -17,22 +17,4 @@ router.get('/', function(req, res) {
 	res.render('index');
 });
 
-// Authenticates the user with Venmo.
-// This function is not used for MVP stage. We had slight complications with Venmo,
-// therefore, moved the functions here and decided to implement the interaction in the final stage.
-router.get('/auth/venmo', passport.authenticate('venmo', {
-    scope: ['make_payments', 'access_feed', 'access_profile', 'access_email', 'access_phone', 'access_balance', 'access_friends'],
-    failureRedirect: '/'
-}), function(req, res) {
-});
-
-// Authenticates the user with Venmo.
-// This function is not used for MVP stage. We had slight complications with Venmo,
-// therefore, moved the functions here and decided to implement the interaction in the final stage.
-router.get('/auth/venmo/callback', passport.authenticate('venmo', {
-    failureRedirect: '/users'
-}), function(req, res) {
-	res.redirect('/users');
-});
-
 module.exports = router;
